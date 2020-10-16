970de95bc6e05d19b7909fccfbf67e3fada91b0c
Valentin Moret* <valentin.moretpro@gmail.com>
2020-10-16 18:15:47 +0200

    add help command with the list of commands

--- service/commands.go
+++ service/commands.go
@@ -2,21 +2,26 @@ package service
 
 import (
 	"github.com/bwmarrin/discordgo"
-
+	"github.com/Clinet/discordgo-embed"
 	"strings"
 )
 
+const helpMessage = "" +
+	"- help: displays a list of commands \n" +
+	"- stats <game>: displays stats of all players from a specific game \n" +
+	"- <player> stats <game>: displays stats of a specific player from a specific game \n"
+
 // RunBot : Create new bot
-func runCommands(s *discordgo.Session, m *discordgo.MessageCreate, args [] string) {
+func runCommands(Session *discordgo.Session, Messager *discordgo.MessageCreate, args [] string) {
 
 	switch strings.ReplaceAll(args[1], " ", "") {
-	case "ping":
-		s.ChannelMessageSend(m.ChannelID, "pong")
+	case "help":
+		Session.ChannelMessageSendEmbed(Messager.ChannelID, embed.NewGenericEmbed("GameStats BOT Helper", helpMessage))
 	case "version":
-		s.ChannelMessageSend(m.ChannelID, "v0.0.1")
+		Session.ChannelMessageSend(Messager.ChannelID, "v0.0.1")
 	case "lol":
-		s.ChannelMessageSend(m.ChannelID, "League of Legends")
+		Session.ChannelMessageSend(Messager.ChannelID, "League of Legends")
 	case "clear":
-		s.ChannelMessageSend(m.ChannelID, "Error, please retry...")
+		Session.ChannelMessageSend(Messager.ChannelID, "Error, please retry...")
 	}
 }
