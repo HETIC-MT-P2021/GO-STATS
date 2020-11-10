c834f1ee9c14d9944029767ce81025c6220e7555
wyllisMonteiro <wyllismonteiro@gmail.com>
2020-11-10 11:11:35 +0100

    refa: set api lol config

--- service/lol.go
+++ service/lol.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"net/http"
-	"os"
 
 	"github.com/yuhanfang/riot/apiclient"
 	"github.com/yuhanfang/riot/constants/region"
@@ -17,34 +17,51 @@ const (
 	reg = region.EUW1
 )
 
-type MessageEmbedImage struct {
-	URL      string `json:"url,omitempty"`
-	ProxyURL string `json:"proxy_url,omitempty"`
-	Width    int    `json:"width,omitempty"`
-	Height   int    `json:"height,omitempty"`
+type ConfigLolAPI struct {
+	RiotGamesToken string
+	Ctx            context.Context
+	Limiter        ratelimit.Limiter
+	Client         apiclient.Client
+	Region         region.Region
+}
+
+var configAPI ConfigLolAPI
+
+func NewConfigLolAPI(riotGamesToken string, reg string) ConfigLolAPI {
+	httpClient := http.DefaultClient
+	limiter := ratelimit.NewLimiter()
+
+	return ConfigLolAPI{
+		RiotGamesToken: riotGamesToken,
+		Ctx:            context.Background(),
+		Limiter:        limiter,
+		Client:         apiclient.New(riotGamesToken, httpClient, limiter),
+		Region:         region.EUW1,
+	}
 }
 
 func GetLolData(username string) (int, string, string, error) {
-
-	key := os.Getenv("RIOTGAMES")
-	httpClient := http.DefaultClient
-	ctx := context.Background()
-	limiter := ratelimit.NewLimiter()
-	client := apiclient.New(key, httpClient, limiter)
+	configAPI = NewConfigLolAPI(os.Getenv("RIOTGAMES"), region.EUW1)
 
 	fmt.Println("GetBySummonerName")
-	summoner, err := client.GetBySummonerName(ctx, reg, username)
+	summoner, err := configAPI.Client.GetBySummonerName(configAPI.Ctx, configAPI.Region, username)
 	if err != nil {
-
 		return 0, "", "", err
 	}
 	prettyPrint(summoner, err)
 
+	//matches := GetMatchList(ctx, reg, summoner.AccountID)
+
 	profileIconID := summoner.ProfileIconID
-	data := fmt.Sprintf("- Level %d", summoner.SummonerLevel)
+	data := fmt.Sprintf("- Level %d", summoner.SummonerLevel /*, matches*/)
 	summonerName := summoner.Name
 
 	return profileIconID, data, summonerName, nil
+}
+
+func GetMatchList(ctx context.Context, r region.Region, accoundID string) string {
+
+	return ""
 }
 
 func prettyPrint(res interface{}, err error) {
